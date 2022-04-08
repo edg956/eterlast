cd4f34ac1da8173936b8a4337b80a211c109fe19
Eugenio <eugeniodonaque@gmail.com>
2022-04-09 01:36:53 +0200

    Don't show staff and superusers on list_users endpoint

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import get_object_or_404
+from django.db.models import Q
 from django.forms.models import model_to_dict
 from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
 
 from users.models import User
@@ -15,7 +16,7 @@ def get_user(request: HttpRequest, id: str):
 
 @require_http_methods(["GET"])
 def list_users(request: HttpRequest):
-    queryset = User.objects.all()
+    queryset = User.objects.filter(Q(is_staff=False) & Q(is_superuser=False))
     return JsonResponse([model_to_dict(nft, fields=["user"]) for nft in queryset], safe=False)
 
 
